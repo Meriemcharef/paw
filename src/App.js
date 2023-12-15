31d43929a306ff9999432765dc7f85617b0ef7c7
Meriemcharef <148784772+Meriemcharef@users.noreply.github.com>
2023-12-15 14:12:19 -0800

    Update App.js
    VOILAAAAAAAAA

--- src/App.js
+++ src/App.js
@@ -1,51 +1,49 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Myday from './pages/Myday';
 import Completed from './pages/Completed';
 import Important from './pages/Important';
-import Religion from './pages/Religion.js';
-import Myweek from './pages/Myweek.js';
-import Mesachats from './pages/Mesachats.js';
+import Task from './pages/Task';
+import All from './pages/All';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
-
 import Login from './pages/Login';
 import { ThemeProvider } from './theme/ThemeContext.js';
 import SignUp from './pages/SignUp';
-import { SignupProvider } from './contexts/SignupContext'; 
-import Counter from './pages/Counter.js';
+import { SignupProvider } from './contexts/SignupContext';
+import ProfilePage from './pages/ProfilePage';
+import MyPlannerComponent from './components/Page.js';
 
-
-
-import './index.css'; 
+import './index.css';
 
 function App() {
+  
   return (
     <ThemeProvider>
-        
       <Router>
-        <div className="App p-10 m-10">
-          <Navbar />
-          <div className="content-wrapper">
-            <div id='side'>
-              <Sidebar />
-            </div>
-            <div id='to'>
-              <Routes>
-                <Route path="/myday" element={<Myday/>} />
-                <Route path="/myweek" element={<Myweek />} />
-                <Route path="/mesAchats" element={<Mesachats />} />
-                {/* <Route path="/completed" element={<Completed />} /> */}
-                <Route path="/important" element={<Important />} />
-                <Route path="/counter" element={<Counter />} />
-                <Route path="/religion" element={<Religion />} />
-
-                <Route path="/login" element={<Login />} />
-                <Route path='/signup' element={<SignUp />} />
-              </Routes>
+        <SignupProvider>
+          <div className="App">
+            <Navbar />
+            <div className="content-wrapper">
+              <div id='side'>
+                <Sidebar />
+              </div>
+              <div id='to'>
+                <Routes>
+                  <Route path="/myday" element={<Myday />} />
+                  <Route path="/task" element={<Task />} />
+                  <Route path="/all" element={<All />} />
+                  <Route path="/completed" element={<Completed />} />
+                  <Route path="/important" element={<Important />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path='/signup' element={<SignUp />} />
+                  <Route path="/profil" element={<ProfilePage />} />
+                </Routes>
+              </div>
             </div>
           </div>
-        </div>
+        </SignupProvider>
       </Router>
     </ThemeProvider>
   );
